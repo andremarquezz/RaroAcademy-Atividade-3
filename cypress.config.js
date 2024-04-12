f45463f6665d6dc459dc6df6ee79643acdd54199
Jey <andre.marqquez@gmail.com>
2024-04-11 23:47:25 -0300

    feat: add baseUrl em cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://raromdb-3c39614e42d4.herokuapp.com/api",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
