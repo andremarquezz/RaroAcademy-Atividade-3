acd44c67f620e67c1c54dfbe74ed481b1cc0d90e
Jey <andre.marqquez@gmail.com>
2024-04-12 16:13:19 -0300

    teste: Criar usuario com sucesso

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://raromdb-3c39614e42d4.herokuapp.com/api",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task",{
+        createRandomUser() {
+          return {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password: "123456",
+          };
+        }
+      })
     },
   },
 });
--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,39 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Criação e consultas de usuarios na API Raro", () => {
+
+  describe("Casos de sucesso", () => {
+
+    it("Deve retornar status 201 e as informações esperadas para um novo usuário criado com sucesso", () => {
+    
+      cy.task("createRandomUser").then((randomUser) => {
+        cy.request("POST", "/users", randomUser).then((responseUserCreated) => {
+          const {body, status} = responseUserCreated;
+     
+          expect(status).to.eq(201);
+          expect(body.id).to.be.a("number");
+          
+          cy.fixture("expectedUserCreatedResponse.json").then((expectedUserCreated) => {
+            expect(body).to.have.all.keys(expectedUserCreated);
+          });
+        });
+      });
+    });
+  
+    // it("Deve retornar status 200 e as informações esperadas para um usuário consultado com sucesso", () => {
+
+    //   cy.task("createRandomUser").then((randomUser) => {
+    //     cy.request("POST", "/users", randomUser).then((responseUserCreated) => {
+    //       const {body, status} = responseUserCreated;
+
+    //       cy.request("GET", `/users/${body.id}`).then((user) => {
+
+    //         expect(user.status).to.eq(200);
+    //         cy.fixture("expectedUserCreatedResponse.json").then((expectedUserCreated) => {
+    //           expect(user.body).to.have.all.keys(expectedUserCreated);
+    //         });
+    //       });
+    //     });
+    //   });
+    // })
+
   })
 })
