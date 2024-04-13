52bbd261906db5046e3ee8da9e4f04d0c1aa9ae0
Jey <andre.marqquez@gmail.com>
2024-04-12 21:36:50 -0300

    refactor: describes

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,25 +1,28 @@
-describe("Criação e consultas de usuarios na API Raro", () => {
-  describe("Casos de sucesso", () => {
-    it("Deve criar um usuário com sucesso e retornar status 201 e o corpo com as informações esperadas.", () => {
-      cy.task("createRandomUser").then((randomUser) => {
+import { mockUser } from "../fixtures/mockUser";
+
+describe("Criação de usuario", () => {
+  describe("Caso de sucesso", () => {
+    it("Deve criar um usuário com sucesso e retornar status 201 e o corpo deve conter as informações esperadas.", () => {
+      cy.createRandomUser().then((randomUser) => {
         cy.request("POST", "/users", randomUser).then((responseUserCreated) => {
           const { body, status } = responseUserCreated;
 
           expect(status).to.eq(201);
           expect(body.id).to.be.a("number");
-
-          cy.fixture("mockUser.json").then((expectedUserCreated) => {
-            expect(body).to.have.all.keys(expectedUserCreated);
-          });
+          expect(body).to.have.all.keys(mockUser.defaultUserData);
         });
       });
     });
+  });
+});
 
-    it("Deve consultar todos os usuários com sucesso e retornar status 200 e uma lista de usuários.", () => {
-      cy.task("createRandomUser").then((randomUser) => {
+describe("Consulta de usuario", () => {
+  describe("Caso de sucesso", () => {
+    it("Deve consultar todos os usuários com sucesso e retornar status 200 e o corpo deve conter uma lista de usuários.", () => {
+      cy.createRandomUser().then((randomUser) => {
         cy.request("POST", "/users", randomUser)
           .then(() => {
-            return cy.request("POST", `/auth/login`, {
+            cy.request("POST", `/auth/login`, {
               email: randomUser.email,
               password: randomUser.password,
             });
@@ -49,6 +52,9 @@ describe("Criação e consultas de usuarios na API Raro", () => {
             const { body, status } = listAllUser;
             expect(status).to.eq(200);
             expect(body).to.be.an("array");
+            body.forEach((user) => {
+              expect(user).to.have.all.keys(mockUser.defaultUserData);
+            });
           });
       });
     });
