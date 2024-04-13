81c980a25528d78cf0d574a404db1e973c3a704b
Jey <andre.marqquez@gmail.com>
2024-04-13 10:20:15 -0300

    teste: adiciona testes auth unauthorized

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -19,7 +19,7 @@ describe("Autenticação", () => {
     });
   });
   describe("Quando a autenticação falha", () => {
-    it("Deve retornar erro 401 (Unauthorized) ao tentar autenticar com email incorreto", () => {
+    it("Deve retornar erro 400 (Bad Request) ao tentar autenticar com email incorreto", () => {
       cy.createRandomUser().then((randomUser) => {
         cy.request("POST", "/users", randomUser).then(() => {
           cy.request({
@@ -34,6 +34,25 @@ describe("Autenticação", () => {
             const { body, status } = response;
             expect(status).to.eq(400);
             expect(body).to.deep.eq(authFixture.errorEmailInvalid);
+          });
+        });
+      });
+    });
+    it("Deve retornar erro 401 (Unauthorized) ao tentar autenticar com a senha incorreta", () => {
+      cy.createRandomUser().then((randomUser) => {
+        cy.request("POST", "/users", randomUser).then(() => {
+          cy.request({
+            method: "POST",
+            url: "/auth/login",
+            failOnStatusCode: false,
+            body: {
+              email: randomUser.email,
+              password: "ruimDeinvadirk",
+            },
+          }).then((response) => {
+            const { body, status } = response;
+            expect(status).to.eq(401);
+            expect(body).to.deep.eq(authFixture.errorUnauthorized);
           });
         });
       });
