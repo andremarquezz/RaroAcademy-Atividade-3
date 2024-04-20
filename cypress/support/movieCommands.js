290d6dff9d599e1dbdf7842e9545cf8c7e5363d4
Jey <andre.marqquez@gmail.com>
2024-04-19 21:37:32 -0300

    refactor: cria arquivos commands user e movie

--- cypress/support/movieCommands.js
+++ cypress/support/movieCommands.js
@@ -1,60 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { movieFixture } from "../fixtures/movieFixture";
 
-Cypress.Commands.add("createRandomUser", () => {
-  return {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    password: "123456",
-  };
-});
-
-Cypress.Commands.add("userLogin", () => {
-  cy.createRandomUser().then((randomUser) => {
-    cy.request("POST", "/users", randomUser)
-      .then((userCreated) => {
-        Cypress.env("currentUser", userCreated.body);
-
-        cy.request("POST", `/auth/login`, {
-          email: randomUser.email,
-          password: randomUser.password,
-        });
-      })
-      .then((userLogged) => {
-        const { accessToken } = userLogged.body;
-        Cypress.env("accessToken", accessToken);
-      });
-  });
-});
-
-Cypress.Commands.add("adminLogin", () => {
-  cy.createRandomUser().then((randomUser) => {
-    cy.request("POST", "/users", randomUser)
-      .then((userCreated) => {
-        userCreated.body.type = 1;
-        Cypress.env("currentUser", userCreated.body);
-
-        cy.request("POST", `/auth/login`, {
-          email: randomUser.email,
-          password: randomUser.password,
-        });
-      })
-      .then((userLogged) => {
-        const { accessToken } = userLogged.body;
-        Cypress.env("accessToken", accessToken);
-      })
-      .then(() => {
-        cy.request({
-          method: "PATCH",
-          url: "/users/admin",
-          headers: {
-            Authorization: `Bearer ${Cypress.env("accessToken")}`,
-          },
-        });
-      });
-  });
-});
-
 Cypress.Commands.add("createRandomMovie", () => {
   const pastDate = faker.date.past();
   const releaseYearFake = pastDate.getFullYear();
